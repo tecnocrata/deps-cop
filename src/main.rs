185f1c31042be99f64d830d11d4b4695923201cf
Enrique Ortuno <enrique@ortuno.net>
2024-04-27 00:57:32 +0000

    multiple command line params added

--- src/main.rs
+++ src/main.rs
@@ -37,26 +37,64 @@ struct CsProject {
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let matches = App::new("Project Dependency Analyzer")
         .version("1.0")
-        .author("Enrique Ortuño <")
+        .author("Your Name")
         .about("Analyzes project dependencies from .csproj files")
         .arg(Arg::new("folder")
              .long("folder")
              .value_name("PATH")
              .help("Sets a custom folder path")
              .takes_value(true))
+        .arg(Arg::new("list")
+             .long("list")
+             .help("Displays all found projects"))
+        .arg(Arg::new("output")
+             .long("output")
+             .value_name("FORMAT")
+             .help("Selects output format ('mermaid' or 'graphviz')")
+             .takes_value(true))
+        .arg(Arg::new("output-html")
+             .long("output-html")
+             .value_name("PATH")
+             .help("Generates an HTML page with the specified output format")
+             .takes_value(true)
+             .requires("output"))
+        .arg(Arg::new("detect-cycles")
+             .long("detect-cycles")
+             .help("Detects cycles in project dependencies"))
         .get_matches();
 
     let root_path = matches.value_of("folder")
-        .map_or_else(|| PathBuf::from("."), PathBuf::from); // Use the current directory if no folder is provided
+        .map_or_else(|| PathBuf::from("."), PathBuf::from);
 
     let projects = collect_projects(&root_path)?;
     let project_dependencies = resolve_dependencies(&projects)?;
 
-    display_project_information(&projects, &project_dependencies);
-    generate_mermaid_diagram(&projects, &project_dependencies);
-    generate_graphviz_diagram(&projects, &project_dependencies);
-    detect_cycles(&projects, &project_dependencies);
+    if matches.is_present("list") {
+        display_project_information(&projects, &project_dependencies);
+    }
 
+    if let Some(format) = matches.value_of("output") {
+        if let Some(html_path) = matches.value_of("output-html") {
+            generate_html_output(&projects, &project_dependencies, html_path, format)?;
+        } else {
+            match format {
+                "mermaid" => generate_mermaid_diagram(&projects, &project_dependencies),
+                "graphviz" => generate_graphviz_diagram(&projects, &project_dependencies),
+                _ => eprintln!("Invalid format. Use 'mermaid' or 'graphviz'."),
+            }
+        }
+    }
+
+    if matches.is_present("detect-cycles") {
+        detect_cycles(&projects, &project_dependencies);
+    }
+
+    Ok(())
+}
+
+fn generate_html_output(projects: &[CsProject], dependencies: &[Vec<usize>], path: &str, format: &str) -> Result<(), Box<dyn std::error::Error>> {
+    println!("Generating HTML output at '{}' using format '{}'", path, format);
+    // Here you would implement the actual logic to generate and save the HTML output.
     Ok(())
 }
 
